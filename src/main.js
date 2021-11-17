b0263b50f8f7ffc4d977f3589a0e13c942017920
何涛 <het@vvtech.io>
2021-11-17 16:42:40 +0800

    引入element-ui，兼容ie及一些配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+import '@babel/polyfill';
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import { 
+	Button,
+	
+} from 'element-ui';
+
+import {message} from './assets/js/message';
+
+Vue.use(Button);
+
+Vue.prototype.$message = message;
 
 Vue.config.productionTip = false;
 
